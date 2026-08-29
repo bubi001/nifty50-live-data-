59e6e581070c6f7fb609e107e9b70eac976548b9
bubi001 <bubibasu35@gmail.com>
2026-08-29 21:41:17 +0530

    Fix response handling and meta extraction in fetch_data.py
    Refactor response handling for fetching data and update meta extraction.

--- fetch_data.py
+++ fetch_data.py
@@ -6,10 +6,10 @@ from datetime import datetime
 # Fetch live Nifty 50 data from a free public endpoint
 url = "https://yahoo.com"
 headers = {'User-Agent': 'Mozilla/5.0'}
-response = requests.get(url, headers=headers).json()
-
+response = requests.get(url, headers=headers)
+data=response.json()
 # Extract real-time metrics safely
-meta = response['chart']['result']['meta']
+meta = response['chart']['result'][0]['meta']
 price = round(meta['regularMarketPrice'], 2)
 prev_close = meta['previousClose']
 change = round(price - prev_close, 2)
@@ -19,9 +19,9 @@ timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 # Create a new row of data
 new_data = pd.DataFrame([[timestamp, "Nifty 50", price, change, pct_change]], 
                         columns=['Timestamp', 'Index', 'Price', 'Change', 'Percent_Change'])
-
-# Append to existing file or create a new one if deleted
 file_path = 'nifty50.csv'
+# Append to existing file or create a new one if deleted
+
 if os.path.exists(file_path):
     df = pd.read_csv(file_path)
     df = pd.concat([df, new_data], ignore_index=True)
